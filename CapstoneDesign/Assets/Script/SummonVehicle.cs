1c56d0d31426539f31f1729cca1734883af5859e
nuonlee <99leesg@gmail.com>
2024-12-14 01:44:34 +0900

    [FIXED]정기 업데이트
    주기적 업데이트

--- CapstoneDesign/Assets/Script/SummonVehicle.cs
+++ CapstoneDesign/Assets/Script/SummonVehicle.cs
@@ -8,18 +8,34 @@ public class SummonVehicle : MonoBehaviour
     public float speed;
     public float distance;
 
+    public Collider[] colliders;
+    public float radius = 10f;
+    public LayerMask layer;
+    private AudioSource audiosource;
+
     Vector3 startpoint;
     // Start is called before the first frame update
     void Start()
     {
         startpoint = new Vector3(x, y, z);
         this.transform.position = startpoint;
+        audiosource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        colliders = Physics.OverlapSphere(transform.position, radius, layer);
+        if (colliders.Length > 0)
+        {
+            foreach(Collider col in colliders)
+            {
+                if(col.tag == "Player")
+                {
+                    audiosource.PlayOneShot(audiosource.clip);
+                }
+            }
+        }
         this.transform.position += new Vector3(0, 0, speed);
         if(speed < 0)
         {
